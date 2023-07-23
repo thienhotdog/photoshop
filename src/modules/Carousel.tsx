52550d01e9504277b7c9d814f408d3e18688c922
Quang Hieu <thienhotdog.com>
2023-07-23 16:41:14 +0700

    update

--- src/modules/Carousel.tsx
+++ src/modules/Carousel.tsx
@@ -1,21 +1,21 @@
 import { DotPosition } from "antd/es/carousel";
 import { useState } from "react";
 import { Carousel, Col, Row } from "antd";
-// import before_1959 from "./../assets/Photo editing/IMG_9159_1.jpg";
-// import after_1959 from "./../assets/Photo editing/IMG_9159.jpg";
-// import after_11245 from "./../assets/Day to Twilight/11245 S Kingston Ave_TBAY-50 - day to dusk.jpg";
-// import before_11245 from "./../assets/Day to Twilight/11245 S Kingston Ave_TBAY-50.jpg";
-// import before_55 from "./../assets/Retouching/Visuals.RealEstate 55.jpg";
-// import after_55 from "./../assets/Retouching/Visuals.RealEstate 56.jpg";
-// import before_1 from "./../assets/Virtual Staging/Virtual Staging/Virtual Staging 02.jpg";
-// import after_1 from "./../assets/Virtual Staging/Virtual Staging/Virtual Staging 01.jpg";
-// import before_product from "./../assets/Clipping Path/PRODUCT SHOOTS3_1.jpg";
-// import after_product from "./../assets/Clipping Path/PRODUCT SHOOTS3.jpg";
+import before_1959 from "./../assets/Photo editing/IMG_9159_1.jpg";
+import after_1959 from "./../assets/Photo editing/IMG_9159.jpg";
+import after_11245 from "./../assets/Day to Twilight/11245.jpg";
+import before_11245 from "./../assets/Day to Twilight/11245S.jpg";
+import before_55 from "./../assets/Retouching/55.jpg";
+import after_55 from "./../assets/Retouching/56.jpg";
+import before_1 from "./../assets/Virtual Staging/Virtual Staging/02.jpg";
+import after_1 from "./../assets/Virtual Staging/Virtual Staging/01.jpg";
+import before_product from "./../assets/Clipping Path/S31.jpg";
+import after_product from "./../assets/Clipping Path/S32.jpg";
 const Carousels = () => {
   return (
     <div>
       <Carousel autoplay dotPosition={"top"}>
-        {/* <div className="w-full  ">
+        <div className="w-full  ">
           <div className="flex justify-between">
             <div className="w-[49.85%]">
               <img src={before_1959} alt="" className="w-full" />
@@ -137,7 +137,7 @@ const Carousels = () => {
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
       </Carousel>
     </div>
   );
