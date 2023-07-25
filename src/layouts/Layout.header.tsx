12989183bcd59b6d627952521e8ceb4059ca111d
Quang Hieu <thienhotdog.com>
2023-07-25 22:04:26 +0700

    update

--- src/layouts/Layout.header.tsx
+++ src/layouts/Layout.header.tsx
@@ -27,130 +27,131 @@ const LayoutHeader = (props: Props) => {
   return (
     <header className={style.header}>
       {/* header */}
-      <div
-        className="py-3 bg-white h-[80px]"
-        style={{ boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 5px" }}
-      >
-        <div className="px-2  gap-x-10	absolute w-full ">
-          <div className="flex justify-between items-center">
-            <ul className="flex cursor-pointer text-[20px] font-bold ">
-              <li>
-                <Link to="/">
-                  <img src={logo} alt="" className="w-[72px]" />
-                </Link>
-              </li>
+      <div className={style.header_1}>
+        <div className={style.header_nav}>
+          <div className="flex items-center">
+            <div className={style.img}>
+              <Link to="">
+                <img src={logo} alt="" className={style.img} />
+              </Link>
+            </div>
+
+            <ul className={style.list_nav_item}>
               {props.title == "" || !props.title ? (
-                <li className="px-10 mx-3 text-green-500	">
+                <li className={style.nav_item}>
                   <Link to="/" onClick={scrollToTop}>
-                    <p className="mt-5 ">HOME</p>
+                    <p>HOME</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black ">
+                <li className={style.nav_item_hover}>
                   <Link to="/" onClick={scrollToTop}>
-                    <p className="mt-5 ">HOME</p>
+                    <p>HOME</p>
                   </Link>
                 </li>
               )}
               {props.title == "COMMERCIAL" ? (
-                <li className="px-10 mx-3 text-green-500   	">
+                <li className={style.nav_item}>
                   <Link to="/servives" onClick={scrollToTop}>
-                    <p className="mt-5">SERVIVES</p>
+                    <p>SERVIVES</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="/servives" onClick={scrollToTop}>
-                    <p className="mt-5">SERVIVES</p>
+                    <p>SERVIVES</p>
                   </Link>
                 </li>
               )}
 
               {props.title == "RENDERS" ? (
-                <li className="px-10 mx-3 text-green-500  	">
+                <li className={style.nav_item}>
                   <Link to="/renders" onClick={scrollToTop}>
-                    <p className="mt-5">HOW IT WORKS</p>
+                    <p>HOW IT WORKS</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="/renders" onClick={scrollToTop}>
-                    <p className="mt-5">HOW IT WORKS</p>
+                    <p>HOW IT WORKS</p>
                   </Link>
                 </li>
               )}
 
               {props.title == "ONLINE" ? (
-                <li className="px-10 mx-3 text-green-500  	">
+                <li className={style.nav_item}>
                   <Link to="/" onClick={scrollToTop}>
-                    <p className="mt-5">FAQ</p>
+                    <p>FAQ</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="/" onClick={scrollToTop}>
-                    <p className="mt-5">FAQ</p>
+                    <p>FAQ</p>
                   </Link>
                 </li>
               )}
               {props.title == "AUTOMOTIVE" ? (
-                <li className="px-10 mx-3 text-green-500  	">
+                <li className={style.nav_item}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">BLOGS</p>
+                    <p>BLOGS</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">BLOGS</p>
+                    <p>BLOGS</p>
                   </Link>
                 </li>
               )}
               {props.title == "ABOUTUS" ? (
-                <li className="px-10 mx-3 text-green-500  	">
+                <li className={style.nav_item}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">ABOUT US</p>
+                    <p>ABOUT US</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">ABOUT US</p>
+                    <p>ABOUT US</p>
                   </Link>
                 </li>
               )}
               {props.title == "CONTACTUS" ? (
-                <li className="px-10 mx-3 text-green-500 	">
+                <li className={style.nav_item}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">CONTACT US</p>
+                    <p>CONTACT US</p>
                   </Link>
                 </li>
               ) : (
-                <li className="px-10 mx-3 hover:text-[#ec1c38] text-black   	">
+                <li className={style.nav_item_hover}>
                   <Link to="" onClick={scrollToTop}>
-                    <p className="mt-5">CONTACT US</p>
+                    <p>CONTACT US</p>
                   </Link>
                 </li>
               )}
             </ul>
+          </div>
 
-            <ul className="flex cursor-pointer items-center	">
-              <li className="text-white">
-                <p className="mt-5">LOGIN</p>
-              </li>
-              <li className="px-4 mx-2 transition duration-300 hover:text-[#ec1c38] hover:bg-gray-600 bg-[#ec1c38] text-white mt-[-5px] h-[36px] w-[120px] text-center">
-                <p className="mt-2">SIGN UP</p>
-              </li>
-              <li className="px-2 mt-[-3px]">
+          <ul className={style.list_nav_item}>
+            <li className={style.nav_item_hover}>
+              <p>LOGIN</p>
+            </li>
+            <li className={style.nav_item_hover}>
+              <p>SIGN UP</p>
+            </li>
+
+            <div className="flex items-center">
+              <Link to="">
                 <img
                   src="https://d1dbtne32ilur4.cloudfront.net/img/flags/US.svg"
                   alt=""
                   width={36}
-                  className="rounded-full"
                 />
-              </li>
-            </ul>
-          </div>
+              </Link>
+              <span className={style.span}> United States</span>
+            </div>
+          </ul>
         </div>
       </div>
       {/* header2 */}
